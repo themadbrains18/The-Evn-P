4f524f7ce1ac2f1a0e34f25eeff3de8d977e5b62
PankajTMB <pankaj@themadbrains.com>
2022-03-30 18:01:58 +0530

    set filter icon on collepse na

--- src/components/SideNav/NavItemAccordian.tsx
+++ src/components/SideNav/NavItemAccordian.tsx
@@ -1,3 +1,5 @@
+
+import { useState } from 'react';
 import { Box } from '@mui/material';
 import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
@@ -6,7 +8,7 @@ import Typography from '@mui/material/Typography';
 import { makeStyles } from '@mui/styles';
 import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
-import Zoom from '@mui/material/Zoom';
+import ClickAwayListener from '@mui/material/ClickAwayListener';
 
 type toogleCheck = {
     checkToggle: Boolean
@@ -28,7 +30,7 @@ const useStyles = makeStyles({
             margin: "0!important",
 
         },
-        "&:hover,&.active,&.Mui-expanded": {
+        "&:hover,&.active,&.Mui-expanded,&[aria-labelledby*='mui-']": {
             backgroundColor: "#F2F6FE!important",
             "& p": {
                 fontWeight: "600",
@@ -41,11 +43,13 @@ const useStyles = makeStyles({
                 stroke: "#0F75BC"
             },
         }
+        
     },
     nav_list_item_acc: {
         justifyContent: "flex-start!important",
         minHeight: "auto!important",
         padding: "8px 10px!important",
+        margin: "0!important"
     },
     filter_label: {
         color: "#8794C3",
@@ -84,92 +88,107 @@ const useStyles = makeStyles({
         },
     },
 });
+
 const NavItemAccordian = (props: toogleCheck) => {
-    // 
     const classes = useStyles();
+    const [open, setOpen] = useState(false);
+
+    const handleTooltipClose = () => {
+        setOpen(false);
+    };
+    const handleTooltipOpen = () => {
+        setOpen(true);
+    };
+
+    if(!props.checkToggle){
+        if(open){
+            handleTooltipClose();
+        }
+    }
     return (
         <>
+
             <Box>
-                <Accordion sx={{ boxShadow: "none" }} className={classes.nav_accordian}>
-                    <AccordionSummary className={classes.nav_list_item_acc}
-                        expandIcon={<svg width={14} className="nav_list_item_arrow" height={8} viewBox="0 0 14 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M1 1.5L7 6.5L13 1.5" stroke="#8794C4" strokeWidth="1.5" strokeLinecap="round" />
-                        </svg>}
-                        aria-controls="panel1a-content"
-                        id="panel1a-header">
-                        <Box sx={{ display: "flex", alignItems: "center", }}>
-                            {props.checkToggle && (
-                                <Tooltip TransitionComponent={Zoom} placement="right" title={
-                                    <Box sx={{ background: "#fff!important", padding: "12px 10px", borderRadius: " 4px" }}>
-                                        <form>
-                                            <Box >
-                                                <Typography component="h4" sx={{ color: "#0F75BC", fontWeight: "600", fontSize: "14px", lineHeight: "16px", marginBottom: "31px" }}>
-                                                    Filter
-                                                </Typography>
-                                                <Box className={classes.filter_input} >
-                                                    <label className={classes.filter_label} htmlFor="tooltipfrom" >From</label>
-                                                    <input className={classes.tool_tipinput} type="date" id="tooltipfrom" name="trip-start" defaultValue="dd/mm/yyy" min="01-01-1999" max="01-01-2030" />
-                                                </Box>
-                                                <Box className={classes.filter_input} sx={{ marginTop: "8px" }} >
-                                                    <label className={classes.filter_label} htmlFor="to">From</label>
-                                                    <input className={classes.tool_tipinput} type="date" id="to" name="trip-start" defaultValue="dd/mm/yyy" min="01-01-1999" max="01-01-2030" />
-                                                </Box>
-                                            </Box>
-                                            <Button variant="contained" sx={{
-                                                width: "100%", marginTop: "10px", padding: "12.5px 0", fontWeight: "400", fontSize: " 12px", lineHeight: "13px", color: "#FFFFFF", background: "#0F75BC"
-                                            }} >Apply Filter</Button>
-                                        </form>
-                                    </Box>
-                                }>
-                                    <span>
-                                        <Button >
-                                            <Box sx={{ mr: 2 }}>
-                                                <svg width={34} height={34} className="filtor_icon" viewBox="0 0 34 34" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                    <path d="M11.1795 19.457H22.8205L25.6395 14.543H8.36056L11.1795 19.457ZM29.2221 5.11328H4.77794C3.96446 5.11328 3.45646 5.9998 3.86485 6.70703L7.13868 12.418H26.8613L30.1385 6.70703C30.5436 5.9998 30.0356 5.11328 29.2221 5.11328V5.11328ZM11.5879 27.8242C11.5879 28.4119 12.0594 28.8867 12.6438 28.8867H21.3563C21.9406 28.8867 22.4121 28.4119 22.4121 27.8242V21.582H11.5879V27.8242Z" fill="#8794C4" />
-                                                </svg>
-                                            </Box>
-                                        </Button>
-                                    </span>
-                                </Tooltip>
-                            )
-                            }
-                            {!props.checkToggle && (
-                                <Button >
-                                    <Box sx={{ mr: 2 }}>
-                                        <svg width={34} height={34} className="filtor_icon" viewBox="0 0 34 34" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                            <path d="M11.1795 19.457H22.8205L25.6395 14.543H8.36056L11.1795 19.457ZM29.2221 5.11328H4.77794C3.96446 5.11328 3.45646 5.9998 3.86485 6.70703L7.13868 12.418H26.8613L30.1385 6.70703C30.5436 5.9998 30.0356 5.11328 29.2221 5.11328V5.11328ZM11.5879 27.8242C11.5879 28.4119 12.0594 28.8867 12.6438 28.8867H21.3563C21.9406 28.8867 22.4121 28.4119 22.4121 27.8242V21.582H11.5879V27.8242Z" fill="#8794C4" />
-                                        </svg>
-                                    </Box>
-                                </Button>
-                            )}
-
-                            <Typography className={classes.filter_acc} >Filters</Typography>
-                        </Box>
-
-                    </AccordionSummary>
-                    {!props.checkToggle && (
-                        <AccordionDetails >
+                {props.checkToggle && (
+                    <ClickAwayListener onClickAway={handleTooltipClose}>
+                        <Tooltip
+                            placement="right"
+                            PopperProps={{
+                                disablePortal: true,
+                            }}
+                            onClose={handleTooltipClose}
+                            open={open}
+                            disableFocusListener
+                            disableHoverListener
+                            disableTouchListener
+                            className={classes.nav_list_item_acc}
+                            title={<Box sx={{ background: "#fff", padding: "12px 10px", borderRadius: " 4px" }}>
                             <form>
-                                <Box>
+                                <Box >
+                                    <Typography component="h4" sx={{ color: "#0F75BC", fontWeight: "600", fontSize: "14px", lineHeight: "16px", marginBottom: "31px" }}>
+                                        Filter
+                                    </Typography>
                                     <Box className={classes.filter_input} >
-                                        <label className={classes.filter_label} htmlFor="from" >From</label>
-                                        <input className={classes.input} type="date" id="from" name="trip-start" defaultValue="dd/mm/yyy" min="01-01-1999" max="01-01-2030" />
+                                        <label className={classes.filter_label} htmlFor="tooltipfrom" >From</label>
+                                        <input className={classes.tool_tipinput} type="date" id="tooltipfrom" name="trip-start" defaultValue="dd/mm/yyy" min="01-01-1999" max="01-01-2030" />
                                     </Box>
                                     <Box className={classes.filter_input} sx={{ marginTop: "8px" }} >
                                         <label className={classes.filter_label} htmlFor="to">From</label>
-                                        <input className={classes.input} type="date" id="to" name="trip-start" defaultValue="dd/mm/yyy" min="01-01-1999" max="01-01-2030" />
+                                        <input className={classes.tool_tipinput} type="date" id="to" name="trip-start" defaultValue="dd/mm/yyy" min="01-01-1999" max="01-01-2030" />
                                     </Box>
                                 </Box>
                                 <Button variant="contained" sx={{
                                     width: "100%", marginTop: "10px", padding: "12.5px 0", fontWeight: "400", fontSize: " 12px", lineHeight: "13px", color: "#FFFFFF", background: "#0F75BC"
                                 }} >Apply Filter</Button>
                             </form>
+                        </Box>}
+                        >
+                            <Button onClick={handleTooltipOpen} sx={{ mr: 2, display: "grid", placeItems: "center",minWidth:"auto" }} className={classes.nav_accordian} >
+                                <svg width={34} height={34} className="filtor_icon" viewBox="0 0 34 34" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M11.1795 19.457H22.8205L25.6395 14.543H8.36056L11.1795 19.457ZM29.2221 5.11328H4.77794C3.96446 5.11328 3.45646 5.9998 3.86485 6.70703L7.13868 12.418H26.8613L30.1385 6.70703C30.5436 5.9998 30.0356 5.11328 29.2221 5.11328V5.11328ZM11.5879 27.8242C11.5879 28.4119 12.0594 28.8867 12.6438 28.8867H21.3563C21.9406 28.8867 22.4121 28.4119 22.4121 27.8242V21.582H11.5879V27.8242Z" fill="#8794C4" />
+                                </svg>
+                            </Button>
+                        </Tooltip>
 
+                    </ClickAwayListener>
+                )}
+
+                {!props.checkToggle && (
+                    <Accordion sx={{ boxShadow: "none" }} className={classes.nav_accordian}>
+                        <AccordionSummary className={classes.nav_list_item_acc}
+                            expandIcon={<svg width={14} className="nav_list_item_arrow" height={8} viewBox="0 0 14 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M1 1.5L7 6.5L13 1.5" stroke="#8794C4" strokeWidth="1.5" strokeLinecap="round" />
+                            </svg>}>
+                            <Box sx={{ display: "flex", alignItems: "center", justifyContent: "flex-start" }}>
+                                <Box sx={{ mr: 2, display: "grid", placeItems: "center" }} >
+                                    <svg width={34} height={34} className="filtor_icon" viewBox="0 0 34 34" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M11.1795 19.457H22.8205L25.6395 14.543H8.36056L11.1795 19.457ZM29.2221 5.11328H4.77794C3.96446 5.11328 3.45646 5.9998 3.86485 6.70703L7.13868 12.418H26.8613L30.1385 6.70703C30.5436 5.9998 30.0356 5.11328 29.2221 5.11328V5.11328ZM11.5879 27.8242C11.5879 28.4119 12.0594 28.8867 12.6438 28.8867H21.3563C21.9406 28.8867 22.4121 28.4119 22.4121 27.8242V21.582H11.5879V27.8242Z" fill="#8794C4" />
+                                    </svg>
+                                </Box>
+                                <Typography className={classes.filter_acc} >Filters</Typography>
+                            </Box>
+                        </AccordionSummary>
+                        
+                        <AccordionDetails >
+                            <Box component="form">
+                                <Box>
+                                    <Box className={classes.filter_input} >
+                                        <label className={classes.filter_label} htmlFor="from" >From</label>
+                                        <input className={classes.input} type="date" id="from" name="trip-start" defaultValue="dd/mm/yyy" min="01-01-1999" max="01-01-2030" />
+                                    </Box>
+                                    <Box className={classes.filter_input} sx={{ marginTop: "8px" }} >
+                                        <label className={classes.filter_label} htmlFor="to">To</label>
+                                        <input className={classes.input} type="date" id="to" name="trip-start" defaultValue="dd/mm/yyy" min="01-01-1999" max="01-01-2030" />
+                                    </Box>
+                                </Box>
+                                <Button variant="contained" sx={{
+                                    width: "100%", marginTop: "10px", padding: "12.5px 0", fontWeight: "400", fontSize: " 12px", lineHeight: "13px", color: "#FFFFFF", background: "#0F75BC"
+                                }} >Apply Filter</Button>
+                            </Box>
                         </AccordionDetails>
-                    )}
+                    </Accordion >
+                )}
 
-
-                </Accordion >
             </Box >
         </>
     )
