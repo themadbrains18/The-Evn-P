bd445c1a89ede5c531276ef8f91b60b347ed7512
pooran das <vermadinesh76097@gmail.com>
2022-04-01 20:15:11 +0530

    Update Code Search dropdown

--- src/components/Maintab/map-tab/map-inner-tabs/location-finder-tab/LocationFinderTab.tsx
+++ src/components/Maintab/map-tab/map-inner-tabs/location-finder-tab/LocationFinderTab.tsx
@@ -4,9 +4,8 @@ import IconButton from '@mui/material/IconButton';
 import Button from '@mui/material/Button';
 import { Typography, Box, Divider, List } from '@mui/material';
 import MapInnerTabHeading from "../mapInnerTabHeading";
-import LocationSearch from "./cationSearch";
+import LocationSearch from "./locationSearch";
 import SearchItems from "./searchItems";
-
 import { makeStyles } from '@mui/styles';
 import { useState } from 'react';
 
@@ -38,6 +37,17 @@ const useStyles = makeStyles({
 
         display: "block"
     },
+    dropdown_items: {
+        position: "absolute",
+        top: "150px",
+        background: "#fff",
+        width: "100%",
+        left: "50%",
+        transform: "translateX(-50%)",
+        boxShadow: "0 0 10px #ccc",
+        borderRadius: "4px",
+        padding: "10px",
+    },
     browse_heading: {
     },
     input_browse: {
@@ -51,26 +61,38 @@ const LocationFinderTab = () => {
         SetsearchValue(e.target.value)
     }
     const submitBtn = () => {
-        setitemvalue((olditems:any) => {
+        setitemvalue((olditems: any) => {
             return [...olditems, searchValue]
         })
         SetsearchValue("")
     }
-    const SearchData = [
-        {
-            itemsNumber:"kh"
-        }
-    ]
-    
     const classes = useStyles();
 
+    const clearData = (id: any) => {
+        if (id !== 'undefined') {
+            var array = [...itemoutput];
+            var index = array.indexOf(id)
+            if (index !== -1) {
+                array.splice(index, 1);
+                setitemvalue(array);
+            }
+        }
+        else {
+            setitemvalue([]);
+        }
+    }
+    // Add Remove Dropdown
+    const [removeDropdown, SetremoveDropdown] = useState(false);
+    const opendropdownBtn = () => {
+        SetremoveDropdown(!removeDropdown)
+    }
     return (
         <>
-            <Box>
+            <Box sx={{ position: "relative" }}>
                 <MapInnerTabHeading heading={"Location Finder"} />
                 <Typography className={classes.map_location_heading} component={"h4"} sx={{ fontSize: "14px", marginTop: "22px", lineHeight: "16px", fontWeight: "500", color: "#000000", marginBottom: "10px", }} >Location Search</Typography>
                 <Paper component="form" sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: "100%", border: "1px solid rgba(0, 0, 0, 0.12)", boxShadow: "none" }}>
-                    <InputBase onChange={searchBtn} value={searchValue} sx={{ ml: 1, flex: 1, fontWeight: "500", fontSize: "14px", lineHeight: " 16px" }} placeholder="Search a location...." inputProps={{ 'aria-label': 'Search a location....' }} />
+                    <InputBase onClick={opendropdownBtn} onChange={searchBtn} value={searchValue} sx={{ ml: 1, flex: 1, fontWeight: "500", fontSize: "14px", lineHeight: " 16px" }} placeholder="Search a location...." inputProps={{ 'aria-label': 'Search a location....' }} />
                     <IconButton onClick={submitBtn} type="submit" sx={{ p: '11px' }} aria-label="search">
                         <svg width={20} height={20} viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M19 19L13 13L19 19ZM15 8C15 8.91925 14.8189 9.82951 14.4672 10.6788C14.1154 11.5281 13.5998 12.2997 12.9497 12.9497C12.2997 13.5998 11.5281 14.1154 10.6788 14.4672C9.82951 14.8189 8.91925 15 8 15C7.08075 15 6.1705 14.8189 5.32122 14.4672C4.47194 14.1154 3.70026 13.5998 3.05025 12.9497C2.40024 12.2997 1.88463 11.5281 1.53284 10.6788C1.18106 9.82951 1 8.91925 1 8C1 6.14348 1.7375 4.36301 3.05025 3.05025C4.36301 1.7375 6.14348 1 8 1C9.85652 1 11.637 1.7375 12.9497 3.05025C14.2625 4.36301 15 6.14348 15 8Z" stroke="#333333" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
@@ -129,20 +151,29 @@ const LocationFinderTab = () => {
                         lineHeight: "1",
                     }} variant="outlined">Go</Button>
                 </Box>
-                {/* Search Options */}
-                <LocationSearch itemsNumber={itemoutput.length}
-                />
-                <List component="ul">
-                    {itemoutput.map((itemvalue, index) => {
-                        return (
-                            <SearchItems
-                                key={index}
-                                searcHeading={itemvalue}
+                {
+                    removeDropdown && (
+                        <Box className={classes.dropdown_items}>
+                            {/* Search Options */}
+                            <LocationSearch itemsNumber={itemoutput.length} clearData={clearData}
                             />
-                        )
-                    })
-                    },
-                </List>
+                            <List component="ul">
+                                {itemoutput.map((itemvalue, index) => {
+                                    return (
+                                        <SearchItems
+                                            key={index}
+                                            recordid={index}
+                                            searcHeading={itemvalue}
+                                            clearData={clearData}
+                                        />
+                                    )
+                                })
+                                }
+                            </List>
+                        </Box>
+                    )
+                }
+
             </Box>
         </>
     );
